d9e1b137efcf5bc6f3daf0171a1889b66cfe6bb7
NanDev <renandatarde@gmail.com>
2024-01-03 22:42:11 -0300

    commit ex14

--- Aula14 ex/ex017/script.js
+++ Aula14 ex/ex017/script.js
@@ -1,14 +1,31 @@
-function contar(){
-    var nstart = document.getElementById('txt1')
-    var nend = document.getElementById('txt2')
-    var npasso = document.getElementById('txt3')
-    var res = document.getElementById('contar')
-    var c = Number(nstart.value)
-    var cend = Number(nend.value)
-    var cpasso = Number(npasso.value) 
-    if (c < cend){
-        for(c;cend;cpasso)
-        res.innerHTML = `${c}`
+function contagem(){
+    var inicio = document.getElementById('txt1')
+    var fim = document.getElementById('txt2')
+    var passo = document.getElementById('txt3')
+    var resp = document.getElementById('contar')
+    var inif = Number(inicio.value)
+    var fimf = Number(fim.value)
+    var passof = Number(passo.value)
+    resp.innerHTML = 'Contando <br>'
+    if (passof == 0){
+        passof = 1
+        alert('Não há Passos informados, considerando 1...')
     }
-    
+    if (inicio.value.length == 0 || fim.value.length == 0){
+        window.alert('[ERRO] Faltou dados!')
+        resp.innerHTML = 'Não é possível contar[...]'
+    } else if (inif < fimf){ 
+        //contagem constante
+        for (var c = inif; c <= fimf; c += passof){
+            resp.innerHTML += `${c} \u{1F449}`
+        }
+    } else {
+        //contagem regressiva
+        for (var c = inif ;c >= fimf ; c -= passof){
+            resp.innerHTML += `${c} \u{1F449}`
+        }
+        
+    }
+        resp.innerHTML += `\u{1F3C1}`
+
 }
