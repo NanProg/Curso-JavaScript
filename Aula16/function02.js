487b371b0bdcfb31bd2c58519da9cd2e53f8cc9a
NanDev <renandatarde@gmail.com>
2024-01-05 22:23:01 -0300

    commit aula16

---
+++ Aula16/function02.js
@@ -0,0 +1,14 @@
+function somar(n1=0, n2=0){
+    return n1 + n2
+    
+}
+console.log(`${somar(2, 3)}`)
+
+/*
+let num = [2,2]
+for (var c=1;c<=10;c++) {
+    for (num[c] = 2;num[c] <= 8; num[c]++){
+        console.log(`[${num[c]}]`) 
+    }
+}
+*/
